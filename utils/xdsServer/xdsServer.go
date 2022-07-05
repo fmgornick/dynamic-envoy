76e006a2861b3542f72994774469ffd4a32aff30
Fletcher Gornick <Fletcher.Gornick@target.com>
2022-07-05 16:05:01 -0500

    removed unneaded commented out code

--- utils/xdsServer/xdsServer.go
+++ utils/xdsServer/xdsServer.go
@@ -8,24 +8,18 @@ import (
 	grpc "google.golang.org/grpc"
 
 	clusterservice "github.com/envoyproxy/go-control-plane/envoy/service/cluster/v3"
-	// discoverygrpc "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	endpointservice "github.com/envoyproxy/go-control-plane/envoy/service/endpoint/v3"
 	listenerservice "github.com/envoyproxy/go-control-plane/envoy/service/listener/v3"
 	routeservice "github.com/envoyproxy/go-control-plane/envoy/service/route/v3"
-	// runtimeservice "github.com/envoyproxy/go-control-plane/envoy/service/runtime/v3"
-	// secretservice "github.com/envoyproxy/go-control-plane/envoy/service/secret/v3"
 	server "github.com/envoyproxy/go-control-plane/pkg/server/v3"
 )
 
 // register services
 func registerServer(grpcServer *grpc.Server, server server.Server) {
-	// discoverygrpc.RegisterAggregatedDiscoveryServiceServer(grpcServer, server)
 	listenerservice.RegisterListenerDiscoveryServiceServer(grpcServer, server)
 	clusterservice.RegisterClusterDiscoveryServiceServer(grpcServer, server)
 	routeservice.RegisterRouteDiscoveryServiceServer(grpcServer, server)
 	endpointservice.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
-	// secretservice.RegisterSecretDiscoveryServiceServer(grpcServer, server)
-	// runtimeservice.RegisterRuntimeDiscoveryServiceServer(grpcServer, server)
 }
 
 // start xds server on given port
@@ -43,5 +37,4 @@ func RunServer(ctx context.Context, server server.Server, port uint) {
 	if err = grpcServer.Serve(lis); err != nil {
 		panic(err)
 	}
-	fmt.Printf("xds server listening on port %d...\n\n", port)
 }
