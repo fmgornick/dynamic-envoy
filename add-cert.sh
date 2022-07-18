b6f985f47c6e52359eefc62f0c9c9680b96922c9
Fletcher Gornick <Fletcher.Gornick@target.com>
2022-07-18 14:55:58 -0500

    apparently you need to swap ' with "

--- add-cert.sh
+++ add-cert.sh
@@ -2,4 +2,4 @@
 DOMAIN=$1
 NAME=$(echo $DOMAIN | cut -d '.' -f 1)
 mkdir -p certs
-openssl req -subj '/CN=$DOMAIN' -new -newkey rsa:4096 -sha256 -days 365 -nodes -x509 -keyout certs/$NAME.key -out certs/$NAME.crt
+openssl req -subj "/CN=$DOMAIN" -new -newkey rsa:4096 -sha256 -days 365 -nodes -x509 -keyout certs/$NAME.key -out certs/$NAME.crt
