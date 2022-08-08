0d9b4d1ae7a9b0619112d3f2a6c527cb0c42a198
Fletcher Gornick <Fletcher.Gornick@target.com>
2022-08-08 12:48:04 -0500

    removed unnecessary line

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,3 @@ tmux new-session -d "envoy -c bootstrap/local.yml"
 tmux split-window -h "./dynamic-proxy -add-http=$HTTP -dir $DIR -ia $INTERNAL_ADDRESS -ip $INTERNAL_PORT -icn $INTERNAL_CNAME -ea $EXTERNAL_ADDRESS -ep $EXTERNAL_PORT -ecn $EXTERNAL_CNAME"
 tmux split-window -v -c "#{pane_current_path}/databags"
 tmux -2 attach-session -d
-cd databags
